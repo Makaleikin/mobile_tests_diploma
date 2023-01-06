3fe37a44e21b95dfc1caa274989fe3776b049abf
Artem <armlynskii@gmail.com>
2023-01-06 18:48:14 +0700

    initial commit

---
+++ wikipedia_app/assist/selectors_xpath.py
@@ -0,0 +1,15 @@
+settings_xpath = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget' \
+                 '.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[' \
+                 '2]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout' \
+                 '/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[' \
+                 '2]/android.widget.RelativeLayout '
+
+add_language = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android' \
+               '.view.ViewGroup/android.widget.FrameLayout[' \
+               '2]/android.widget.FrameLayout/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView' \
+               '/android.widget.LinearLayout[3]/android.widget.TextView '
+
+russian_language = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout' \
+                   '/android.view.ViewGroup/android.widget.FrameLayout[' \
+                   '2]/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget' \
+                   '.LinearLayout[3] '
